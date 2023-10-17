e3c0bc37f6ba373f3bfce63c7ee5d425b6a7d9f5
Mouri69 <147693463+Mouri69@users.noreply.github.com>
2023-10-17 13:11:58 +0300

    Update contact_form.php

--- contact_form.php
+++ contact_form.php
@@ -1,13 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $content = $_POST["content"];
+    // Sanitize the form data to prevent potential security issues
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
     // Format the email message
-    $message = "Name: $name\nEmail: $email\nSubject: $subject\nContent: $content";
+    $email_content = "Name: $name\nEmail: $email\nMessage: $message";
 
     // Set the recipient email address
     $to = "trident_mazen@outlook.com"; // Replace with your email address
@@ -15,12 +14,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Set the email subject
     $email_subject = "New Contact Form Submission";
 
-    // Set additional headers
-    $headers = "From: $email\r\n";
+    // Set additional headers, including MIME version and content type
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/plain;charset=UTF-8" . "\r\n";
+    $headers .= "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
 
+    // Mail sending parameters
+    $additional_parameters = "-f" . $email;
+
     // Send the email
-    $mail_sent = mail($to, $email_subject, $message, $headers);
+    $mail_sent = mail($to, $email_subject, $email_content, $headers, $additional_parameters);
 
     // Check if the email was sent successfully
     if ($mail_sent) {
